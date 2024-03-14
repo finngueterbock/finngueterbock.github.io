e7c04cbf2c356165f588c66760c7e3b60f27327e
finngueterbock <wu19361@bristol.ac.uk>
2024-03-14 20:06:54 +0000

    new font and blur effect

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-// Example of a simple animation using GSAP (make sure to include the GSAP library)
-const images = document.querySelectorAll('.image-gallery img');
+// // Example of a simple animation using GSAP (make sure to include the GSAP library)
+// const images = document.querySelectorAll('.image-gallery img');
 
-images.forEach(image => {
-    gsap.from(image, { 
-        opacity: 0, 
-        y: 50, 
-        duration: 1, 
-        ease: 'power2.out',
-        stagger: 0.2 
-    });
-});
+// images.forEach(image => {
+//     gsap.from(image, { 
+//         opacity: 0, 
+//         y: 50, 
+//         duration: 1, 
+//         ease: 'power2.out',
+//         stagger: 0.2 
+//     });
+// });
